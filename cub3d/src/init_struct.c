5eb1fa02a9b38ee46f9223167806d5c46d1828ac
Morty <yaya@MortyWick.localdomain>
2026-02-04 15:29:44 +0100

    parse01

--- cub3d/src/init_struct.c
+++ cub3d/src/init_struct.c
@@ -12,104 +12,141 @@
 
 #include <cub3d.h>
 
-int is_map_line(char *line)
+static int	is_map_char(char c)
 {
-    int i = 0;
-
-    while (line[i])
-    {
-        if (line[i] == '1' || line[i] == '0')
-            return (1);
-        i++;
-    }
-    return (0);
+	return (c == '0' || c == '1' || c == 'N' || c == 'S' || c == 'E'
+		|| c == 'W' || c == ' ');
 }
 
-int count_map_height(int fd)
+int	is_map_line(char *line)
 {
-    char *line;
-    int height = 0;
-    int in_map = 0;
+	int	i;
 
-    while ((line = get_next_line(fd)))
-    {
-        if (!in_map && is_map_line(line))
-            in_map = 1;
-        if (in_map)
-            height++;
-        free(line);
-    }
-    return (height);
+	if (!line)
+		return (0);
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] == '\n' || line[i] == '\r')
+			break ;
+		if (is_map_char(line[i]) && line[i] != ' ')
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-void alloc_map(t_map *map, int height)
+int	count_map_height(int fd)
 {
-    map->grid = malloc(sizeof(char *) * (height + 1));
-    map->height = height;
-    map->width = 0;
+	char	*line;
+	int		height;
+	int		in_map;
+
+	height = 0;
+	in_map = 0;
+	while ((line = get_next_line(fd)))
+	{
+		if (!in_map && is_map_line(line))
+			in_map = 1;
+		if (in_map)
+		{
+			if (!is_map_line(line))
+			{
+				free(line);
+				break ;
+			}
+			height++;
+		}
+		free(line);
+	}
+	return (height);
 }
 
-void    fill_map(t_map *map, int fd)
+void	alloc_map(t_map *map, int height)
 {
-    char *line;
-    int i = 0;
-    int in_map = 0;
-
-    while ((line = get_next_line(fd)))
-    {
-        if(!in_map && is_map_line(line))
-            in_map = 1;
-        if(in_map)
-        {
-            map->grid[i] = ft_strtrim(line, "\n");
-            if((int)ft_strlen(map->grid[i]) > map->width)
-                map->width = ft_strlen(map->grid[i]);
-            i++;
-        }
-        free(line);
-    }
-    map->grid[i] = NULL;
-    
+	map->grid = malloc(sizeof(char *) * (height + 1));
+	map->height = height;
+	map->width = 0;
 }
 
-void    find_player(t_map *map)
+static char	*trim_eol(char *line)
 {
-    int y = 0;
-    int x;
+	size_t	len;
 
-    while(map->grid[y])
-    {
-        x = 0;
-        while(map->grid[y][x])
-        {
-            if(ft_strchr("NSEW", map->grid[y][x]))
-            {
-                map->player_dir = map->grid[y][x];
-                map->player_x = x;
-                map->player_y = y;
-
-                return;
-            }
-            x++;
-        }
-        y++;
-    }
+	if (!line)
+		return (NULL);
+	len = ft_strlen(line);
+	while (len > 0 && (line[len - 1] == '\n' || line[len - 1] == '\r'))
+		len--;
+	line[len] = '\0';
+	return (line);
 }
 
-void    init_map(t_map *map, char *path_map)
+void	fill_map(t_map *map, int fd)
 {
-    int fd;
-    int height;
+	char	*line;
+	int		i;
+	int		in_map;
 
-    fd = open(path_map, O_RDONLY);
-    height = count_map_height(fd);
-    close(fd);
+	i = 0;
+	in_map = 0;
+	while ((line = get_next_line(fd)))
+	{
+		if (!in_map && is_map_line(line))
+			in_map = 1;
+		if (in_map)
+		{
+			if (!is_map_line(line))
+			{
+				free(line);
+				break ;
+			}
+			line = trim_eol(line);
+			map->grid[i] = ft_strdup(line);
+			if ((int)ft_strlen(map->grid[i]) > map->width)
+				map->width = ft_strlen(map->grid[i]);
+			i++;
+		}
+		free(line);
+	}
+	map->grid[i] = NULL;
+}
 
-    alloc_map(map, height);
+void	find_player(t_map *map)
+{
+	int	y;
+	int	x;
 
-    fd = open(path_map, O_RDONLY);
-    fill_map(map, fd);
-    close(fd);
+	y = 0;
+	while (map->grid[y])
+	{
+		x = 0;
+		while (map->grid[y][x])
+		{
+			if (ft_strchr("NSEW", map->grid[y][x]))
+			{
+				map->player_dir = map->grid[y][x];
+				map->player_x = x;
+				map->player_y = y;
+				return ;
+			}
+			x++;
+		}
+		y++;
+	}
+}
 
-    find_player(map);
+void	init_map(t_map *map, char *path_map)
+{
+	int	fd;
+	int	height;
+
+	fd = open(path_map, O_RDONLY);
+	height = count_map_height(fd);
+	close(fd);
+	alloc_map(map, height);
+	fd = open(path_map, O_RDONLY);
+	fill_map(map, fd);
+	close(fd);
+	find_player(map);
 }
